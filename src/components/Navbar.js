df6284a134e476583fc69ab86a0b3f37d483758b
Magnuz21 <aldrinchicoteabilar@gmail.com>
2026-08-13 12:10:45 +0800

    last

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar({ link }) {
     
     return (
         <div className="flex gap-5 pb-10 font-bold text-2xl bg-gray-900 text-gray-100">
-            <Link href="/">Homes</Link>
+            <Link href="/">Home</Link>
             <Link href="/about">About Us</Link>
             <Link href="/events">Events</Link>
         </div>
